e47d53129347ee5200043f01e95fab3b9005fcf6
orkankirdar <orkankirdar07@gmail.com>
2025-11-27 23:56:42 +0300

    Add model building function using MobileNetV2

--- Hotdog-Not-Hotdog/src/model.py
+++ Hotdog-Not-Hotdog/src/model.py
@@ -1,1 +1,25 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def build_model(input_shape=(224,224,3)):
+    base_model = keras.applications.MobileNetV2(
+        include_top=False,
+        weights="imagenet",
+        input_shape=input_shape
+    )
+    base_model.trainable = False
+
+    model = keras.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(1, activation='sigmoid')
+    ])
+    
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+    
+    return model
 
