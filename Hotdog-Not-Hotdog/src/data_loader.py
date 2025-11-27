93c97698a59bb7c07ecac55314e8f0b0866362a0
orkankirdar <orkankirdar07@gmail.com>
2025-11-27 23:47:31 +0300

    Update data_loader.py

--- Hotdog-Not-Hotdog/src/data_loader.py
+++ Hotdog-Not-Hotdog/src/data_loader.py
@@ -1,1 +1,11 @@
+import tensorflow as tf
+
+def load_data(data_dir, img_size=(224, 224), batch_size=32):
+    return tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        image_size=img_size,
+        batch_size=batch_size,
+        label_mode='binary',
+        shuffle=True
+    )
 
